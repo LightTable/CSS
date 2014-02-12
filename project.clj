a5273073f5ee769092c3c5aa125a6c853129a0f9
Chris Granger <ibdknox@gmail.com>
2014-02-11 16:30:07 -0800

    update to latest codemirror css mode
    Signed-off-by: Chris Granger <ibdknox@gmail.com>

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
-(defproject com.lighttable/css "0.0.2"
+(defproject com.lighttable/css "0.0.3"
   :description "CSS language plugin for Light Table"
   :dependencies [[org.clojure/clojure "1.5.1"]])
